aeb3735b8315bfe3550a1cbb3eac089a8b65e6a5
Ben Nilsson <55544010+BenNilsson@users.noreply.github.com>
2020-06-25 13:31:55 +0100

    Add files via upload

---
+++ SortItemsByName.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class SortItemsByName : EditorWindow
+{
+    [MenuItem("GameObject/A-Z Sort &s")]
+    public static void SortObjects()
+    {
+        if (Selection.gameObjects.Length > 0)
+        {
+            int initialIndex = Selection.gameObjects[0].transform.GetSiblingIndex();
+
+            // Sort
+            GameObject[] sortedObjects = Selection.gameObjects.OrderBy(go => go.name).ToArray();
+            for (int i = 0; i < sortedObjects.Length; i++)
+            {
+                sortedObjects[i].transform.SetSiblingIndex(initialIndex + i);
+            }
+        }
+        else Debug.Log("No objects selected to sort");
+    }
+}
